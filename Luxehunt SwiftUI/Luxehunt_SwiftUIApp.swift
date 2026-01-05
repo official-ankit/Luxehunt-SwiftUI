8fa8618d06771ba8147588170741af8aa51a12d7
PrateekPo <108451530+PrateekPo@users.noreply.github.com>
2026-01-05 15:41:35 +0530

    Initial Commit

---
+++ Luxehunt SwiftUI/Luxehunt_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Luxehunt_SwiftUIApp.swift
+//  Luxehunt SwiftUI
+//
+//  Created by Codemaya on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Luxehunt_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
